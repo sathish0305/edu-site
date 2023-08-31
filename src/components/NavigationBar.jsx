dc60dcb22eca1554bf33b7bfb1845c8b500b2998
Sathish M <sathishms0305@gmail.com>
2023-08-31 23:13:51 +0530

    home page and navbar correction

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../images/pyLogo.png'
 
 function NavigationBar() {
   return (
@@ -15,17 +14,13 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav">
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link className='nav-link' href="#home">Home</Nav.Link>
             <Nav.Link className='nav-link' href="#about">About</Nav.Link>
             <Nav.Link className='nav-link' href="#courses">Courses</Nav.Link>
+            <Nav.Link className='nav-link' href="#memes">Branches</Nav.Link>
             <Nav.Link className='nav-link' href="#corporates">Corporates</Nav.Link>
-          </Nav>
-          <Nav>
             <Nav.Link className='nav-link' href="#deets">Contact</Nav.Link>
-            <Nav.Link className='nav-link' eventKey={2} href="#memes">
-              Branches
-            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,20 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from '../components/NavigationBar'
 import '../App.css'
+import HomeImage from '../images/computer-engineer.png'
 
 function Home() {
   return (
     <div className='home-page'>
         <NavigationBar />
-        <div className="wave-wrapper">
-           
+        <div className="home-section">
+            <div className='home-text'>
+              <h3>Largest Software Training Institute</h3>
+              <h4>The institute is a platform where young minds are given the opportunity to build successful careers.</h4>
+            </div>
+            <div className='home-image'>
+              <img src={HomeImage} alt="" />
+            </div>
         </div>
     </div>
   )
