356b1a1f855584540cfef696139ade2ffa632c52
Sathish M <sathishms0305@gmail.com>
2023-09-05 22:38:16 +0530

    home page buttons added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,14 @@ function Home() {
         <NavigationBar />
         <div className="home-section">
             <div className='home-text'>
-              <h3>Largest Software Training Institute</h3>
+              <h3><b>Largest Software Training Institute</b></h3>
               <h4>The institute is a platform where young minds are given the opportunity to build successful careers.</h4>
+              <div className='home-buttons'>
+              <a class="button-87">Learn more</a>
+              <a class="button-87">Contact Us</a>
             </div>
+            </div>
+            
             <div className='home-image'>
               <img src={HomeImage} alt="" />
             </div>
