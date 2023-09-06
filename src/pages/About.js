707e684e5935c36fd62253140e16a66630e267de
Sathish M <sathishms0305@gmail.com>
2023-09-06 22:14:25 +0530

    about image updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import HomeImage from '../images/computer-engineer.png'
+import AboutImage from '../images/computer-engineer1.png'
 
 function About() {
   return (
     <>
     <div className='about-page-section'>
       <div className="about-page">
-      <div className="about-image-section">
-        <img src={HomeImage} alt="This is image section" />
-      </div>
+        <div className="about-image-section">
+          <img src={AboutImage} alt="about section" />
+        </div>
       <div className="about-text-section">
         <div className="about-header">
           <h1>About us</h1>
